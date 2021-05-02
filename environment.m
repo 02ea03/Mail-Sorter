34d5010dfaef536195a27b0a79a28414fc2ae0af
PJ-Un <82499541+PJ-Un@users.noreply.github.com>
2021-05-02 16:09:05 +1000

    Add files via upload

--- environment.m
+++ environment.m
@@ -1,10 +1,10 @@
 clf;clc;clear all;close all;
-doCameraSpin = false;
+%doCameraSpin = false;
 
 % Turn on a light (only turn on 1, don't keep turning them on), and make axis equal
 camlight;
 view(3)
-% axis equal
+axis equal;
 hold on;
 
 %% e-stop
