7675b955c22b8be83d07c7546a8d174e89a88595
PJ-Un <82499541+PJ-Un@users.noreply.github.com>
2021-05-04 20:36:25 +1000

    Add files via upload

--- test.m
+++ test.m
@@ -64,3 +64,29 @@ robot.model.animate(q);
 %                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
 %         end
 %     end
+
+
+a = transl(0,0,0);
+b = transl(1,1,1);
+qInitial = zero(1,6);
+steps = 100;
+s = lspb(0,1,steps);
+for i = 1 : size(tr,3)-1    
+    for faceIndex = 1:size(faces,1)
+        vertOnPlane = vertex(faces(faceIndex,1)',:);
+        [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
+        if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
+            plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
+            display('Intersection');
+        end
+    end    
+end
+q0=robot.model.ikcon(a,qInitial);
+q1=robot.model.ikcon(b,qInitial);
+
+for i=1:steps
+    x(i,:) = (1-s(i))*a + s(i)*b;
+end
+
+
+
