6161a9e63dfccead88c4592285c6aadb7d8a95cb
Han Gyo Jung <pj1998paul@gmail.com>
2021-05-13 03:16:10 +1000

    Robot part1 working

--- environment.m
+++ environment.m
@@ -141,7 +141,7 @@ hold on;
 vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
 for zOffset = [3.5]
     for yOffset = [0]
-        for xOffset = [1.4]            
+        for xOffset = [1.5]            
             trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset ...
         ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
         end
@@ -152,18 +152,32 @@ end
 vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
 for zOffset = [3.51]
     for yOffset = [0]
-        for xOffset = [1.4]          
+        for xOffset = [1.5]          
             paperTri([1]) = trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset ... %delete(paperTri([1]) to delete the
         ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
         end
     end
 end
+display('Finished Loading Background')
 %% Robot
-robot = VP6242(false);
-q = deg2rad([0 30 90 -30 0 0]);
-robot.model.base = transl(0,0,3.2);
+%robot = VP6242(false);
+%q = deg2rad([0 0 0 0 0 0]);
+%robot.model.base = transl(0,0,3.2);
 view(-15,21);
-robot.model.animate(q);
+%robot.model.animate(q);
+L1 = Link('d',0.5,'a',0,'alpha',pi/2,'offset',0,'qlim',deg2rad([-200 200]));
+L2 = Link('d',0,'a',0.84,'alpha',0,'offset',pi/2,'qlim',deg2rad([-40 120]));
+L3 = Link('d',0,'a',0.3,'alpha',-pi/2,'offset',0,'qlim',deg2rad([-140 90]));
+L4 = Link('d',0.84,'a',0,'alpha',pi/2,'offset',0,'qlim',deg2rad([-160 160]));
+L5 = Link('d',0,'a',0,'alpha',-pi/2,'offset',0,'qlim',deg2rad([-120 120]));
+L6 = Link('d',0.28,'a',0,'alpha',0,'offset',0,'qlim',deg2rad([-360 360]));
+robot = SerialLink([L1 L2 L3 L4 L5 L6],'name','VP6242');
+q = deg2rad([0 0 0 0 0 0]);
+scale=0.5;
+workspace=[-10 10 -10 10 -0.1 10];
+robot.base = transl(0,0,3.2);
+view(-15,21);
+robot.plot(q,'workspace',workspace,'scale',scale);
 hold on;
 pause(0.01);
 view(-15,21);
@@ -191,21 +205,21 @@ angleError = zeros(3,steps);    % For plotting trajectory error
 % 1.3) Set up trajectory, initial pose
 s = lspb(0,1,steps);                % Trapezoidal trajectory scalar
 
-for i = 1 : size(steps,3)-1            % Collision (steps was tr)
-    for faceIndex = 1:size(faces,1)
-        vertOnPlane = vertex(faces(faceIndex,1)',:);
-        [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,steps(1:3,4,i)',steps(1:3,4,i+1)'); %(steps was tr)
-        if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
-            plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
-            display('Intersection');
-        end
-    end    
-end
+% for i = 1 : size(tr,3)-1            % Collision (steps was tr)
+%     for faceIndex = 1:size(faces,1)
+%         vertOnPlane = vertex(faces(faceIndex,1)',:);
+%         [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); %(steps was tr)
+%         if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
+%             plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
+%             display('Intersection');
+%         end
+%     end    
+% end
 
 for i=1:steps
-    x(1,i) = (1-s(i))*0.6 + s(i)*2; % Points in x
-    x(2,i) = (1-s(i))*0 + s(i)*1; % Points in y
-    x(3,i) = (1-s(i))*5.4 + s(i)*3.6; % Points in z
+    x(1,i) = (1-s(i))*-1.12 + s(i)*1.5  ; % Points in x
+    x(2,i) = (1-s(i))*0 + 2*sin(i*delta/2); % Points in y
+    x(3,i) = (1-s(i))*4.84 + s(i)*4; % Points in z
     theta(1,i) = 0;                 % Roll angle 
     theta(2,i) = 5*pi/9;            % Pitch angle
     theta(3,i) = 0;                 % Yaw angle
@@ -214,11 +228,11 @@ end
  
 T = [rpy2r(theta(1,1),theta(2,1),theta(3,1)) x(:,1);zeros(1,3) 1];          % Create transformation of first point and angle
 q0 = zeros(1,6);                                                            % Initial guess for joint angles
-qMatrix(1,:) = robot.model.ikcon(T,q0);                                            % Solve joint angles to achieve first waypoint
+qMatrix(1,:) = robot.ikcon(T,q0);                                            % Solve joint angles to achieve first waypoint
 
 % 1.4) Track the trajectory with RMRC
 for i = 1:steps-1
-    T = robot.model.fkine(qMatrix(i,:));                                           % Get forward transformation at current joint state
+    T = robot.fkine(qMatrix(i,:));                                           % Get forward transformation at current joint state
     deltaX = x(:,i+1) - T(1:3,4);                                         	% Get position error from next waypoint
     Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
     Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
@@ -228,7 +242,7 @@ for i = 1:steps-1
     angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
     deltaTheta = tr2rpy(Rd*Ra');                                            % Convert rotation matrix to RPY angles
     xdot = W*[linear_velocity;angular_velocity];                          	% Calculate end-effector velocity to reach next waypoint.
-    J = robot.model.jacob0(qMatrix(i,:));                 % Get Jacobian at current joint state
+    J = robot.jacob0(qMatrix(i,:));                 % Get Jacobian at current joint state
     m(i) = sqrt(round(det(J*J')));
     if m(i) < epsilon  % If manipulability is less than given threshold
         lambda = (1 - m(i)/epsilon)*5E-2;
@@ -238,9 +252,9 @@ for i = 1:steps-1
     invJ = inv(J'*J + lambda *eye(6))*J';                                   % DLS Inverse
     qdot(i,:) = (invJ*xdot)';                                                % Solve the RMRC equation (you may need to transpose the         vector)
     for j = 1:6                                                             % Loop through joints 1 to 6
-        if qMatrix(i,j) + deltaT*qdot(i,j) < robot.model.qlim(j,1)                     % If next joint angle is lower than joint limit...
+        if qMatrix(i,j) + deltaT*qdot(i,j) < robot.qlim(j,1)                     % If next joint angle is lower than joint limit...
             qdot(i,j) = 0; % Stop the motor
-        elseif qMatrix(i,j) + deltaT*qdot(i,j) > robot.model.qlim(j,2)                 % If next joint angle is greater than joint limit ...
+        elseif qMatrix(i,j) + deltaT*qdot(i,j) > robot.qlim(j,2)                 % If next joint angle is greater than joint limit ...
             qdot(i,j) = 0; % Stop the motor
         end
     end
@@ -249,20 +263,53 @@ for i = 1:steps-1
     angleError(:,i) = deltaTheta;                                           % For plotting
 end
 % 1.5) Plot the results
-%for 0:1:steps
-robot.model.animate(qMatrix) %(i,:) need for loop
+for i=1:steps-1
+    robot.animate(qMatrix(i,:)); %(i,:) need for loop
 %paperCircle %look video in resources
-%end
+end
 view(-15,21);
 %end
 
-%% Mail to Right Box
+%% Mail to Pink Box
 %how to pick up the mail and move it in motion
+% for i=1:steps
+%     x(1,i) = (1-s(i))*1.5 + s(i)*-1; % Points in x
+%     x(2,i) = (1-s(i))*0 + s(i)*-2; % Points in y
+%     x(3,i) = (1-s(i))*4 + s(i)*3.2; % Points in z
+%     theta(1,i) = 0;                 % Roll angle 
+%     theta(2,i) = 5*pi/9;            % Pitch angle
+%     theta(3,i) = 0;                 % Yaw angle
+%     %x(i,:) = (1-s(i))*POS1 + s(i)*POS2;
+% end
+%% Pink Box to Mail
+% for i=1:steps
+%     x(1,i) = (1-s(i))*-1 + s(i)*1.5; % Points in x
+%     x(2,i) = (1-s(i))*-2 + s(i)*0; % Points in y
+%     x(3,i) = (1-s(i))*3.4 + s(i)*4; % Points in z
+%     theta(1,i) = 0;                 % Roll angle 
+%     theta(2,i) = 5*pi/9;            % Pitch angle
+%     theta(3,i) = 0;                 % Yaw angle
+%     %x(i,:) = (1-s(i))*POS1 + s(i)*POS2;
+% end
 
-%% Right Box to Mail
+%% Mail to Blue Box
+% for i=1:steps
+%     x(1,i) = (1-s(i))*1.5 + s(i)*-1; % Points in x
+%     x(2,i) = (1-s(i))*0 + s(i)*2; % Points in y
+%     x(3,i) = (1-s(i))*4 + s(i)*3.4; % Points in z
+%     theta(1,i) = 0;                 % Roll angle 
+%     theta(2,i) = 5*pi/9;            % Pitch angle
+%     theta(3,i) = 0;                 % Yaw angle
+%     %x(i,:) = (1-s(i))*POS1 + s(i)*POS2;
+% end
 
-
-%% Mail to Left Box
-
-
-%% Left Box to Mail
+%% Left Blue to Mail
+% for i=1:steps
+%     x(1,i) = (1-s(i))*-1 + s(i)*1.5; % Points in x
+%     x(2,i) = (1-s(i))*2 + s(i)*0; % Points in y
+%     x(3,i) = (1-s(i))*3.4 + s(i)*4; % Points in z
+%     theta(1,i) = 0;                 % Roll angle 
+%     theta(2,i) = 5*pi/9;            % Pitch angle
+%     theta(3,i) = 0;                 % Yaw angle
+%     %x(i,:) = (1-s(i))*POS1 + s(i)*POS2;
+% end
