43634c4976c6d66fb55391d13b6e3b8deeff20d8
Han Gyo Jung <pj1998paul@gmail.com>
2021-05-15 05:17:13 +1000

    moves the paper with the robot

--- test_code/test.m
+++ test_code/test.m
@@ -2,91 +2,107 @@ clc;clear all;close all;
 camlight;
 axis equal;
 view(3);
-hold on;
-robot = VP6242(false);
-q = zeros(1,6);
-robot.model.base = transl(0,0,0);
-robot.model.animate(q);
+% hold on;
+% robot = VP6242(false);
+% q = zeros(1,6);
+% robot.model.base = transl(0,0,0);
+% robot.model.animate(q);
+% 
+% 
+% % %% parts check
+% %     [f,v,data] = plyread('VP6242Link0.ply','tri');
+% %     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+% % 
+% %     for xOffset = [0]
+% %         for yOffset = [0]
+% %             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
+% %                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+% %         end
+% %     end
+% %         [f,v,data] = plyread('VP6242Link1.ply','tri');
+% %     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+% % 
+% %     for xOffset = [0]
+% %         for yOffset = [0]
+% %             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
+% %                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+% %         end
+% %     end
+% %         [f,v,data] = plyread('VP6242Link2.ply','tri');
+% %     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+% % 
+% %     for xOffset = [0]
+% %         for yOffset = [0]
+% %             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
+% %                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+% %         end
+% %     end
+% %         [f,v,data] = plyread('VP6242Link3.ply','tri');
+% %     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+% % 
+% %     for xOffset = [0]
+% %         for yOffset = [0]
+% %             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
+% %                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+% %         end
+% %     end
+% %         [f,v,data] = plyread('VP6242Link4.ply','tri');
+% %     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+% % 
+% %     for xOffset = [0]
+% %         for yOffset = [0]
+% %             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
+% %                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+% %         end
+% %     end
+% %         [f,v,data] = plyread('VP6242Link5.ply','tri');
+% %     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+% % 
+% %     for xOffset = [0]
+% %         for yOffset = [0]
+% %             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
+% %                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+% %         end
+% %     end
+% 
+% 
+% a = transl(0,0,0);
+% b = transl(1,1,1);
+% qInitial = zero(1,6);
+% steps = 100;
+% s = lspb(0,1,steps);
+% for i = 1 : size(tr,3)-1    
+%     for faceIndex = 1:size(faces,1)
+%         vertOnPlane = vertex(faces(faceIndex,1)',:);
+%         [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
+%         if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
+%             plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
+%             display('Intersection');
+%         end
+%     end    
+% end
+% q0=robot.model.ikcon(a,qInitial);
+% q1=robot.model.ikcon(b,qInitial);
+% 
+% for i=1:steps
+%     x(i,:) = (1-s(i))*a + s(i)*b;
+% end
 
-
-% %% parts check
-%     [f,v,data] = plyread('VP6242Link0.ply','tri');
-%     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-% 
-%     for xOffset = [0]
-%         for yOffset = [0]
-%             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
-%                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
-%         end
-%     end
-%         [f,v,data] = plyread('VP6242Link1.ply','tri');
-%     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-% 
-%     for xOffset = [0]
-%         for yOffset = [0]
-%             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
-%                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
-%         end
-%     end
-%         [f,v,data] = plyread('VP6242Link2.ply','tri');
-%     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-% 
-%     for xOffset = [0]
-%         for yOffset = [0]
-%             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
-%                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
-%         end
-%     end
-%         [f,v,data] = plyread('VP6242Link3.ply','tri');
-%     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-% 
-%     for xOffset = [0]
-%         for yOffset = [0]
-%             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
-%                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
-%         end
-%     end
-%         [f,v,data] = plyread('VP6242Link4.ply','tri');
-%     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-% 
-%     for xOffset = [0]
-%         for yOffset = [0]
-%             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
-%                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
-%         end
-%     end
-%         [f,v,data] = plyread('VP6242Link5.ply','tri');
-%     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-% 
-%     for xOffset = [0]
-%         for yOffset = [0]
-%             trisurf(f,v(:,1)+ xOffset,v(:,2) + yOffset, v(:,3) ...
-%                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
-%         end
-%     end
-
-
-a = transl(0,0,0);
-b = transl(1,1,1);
-qInitial = zero(1,6);
-steps = 100;
-s = lspb(0,1,steps);
-for i = 1 : size(tr,3)-1    
-    for faceIndex = 1:size(faces,1)
-        vertOnPlane = vertex(faces(faceIndex,1)',:);
-        [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
-        if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
-            plot3(intersectP(1),intersectP(2),intersectP(3),'g*');
-            display('Intersection');
+[f,v,data] = plyread('paperCircle.ply','tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+for zOffset = [3.5]
+    for yOffset = [0]
+        for xOffset = [1.6]            
+            paperCircle([1])= trisurf(f,v(:,1) + xOffset,v(:,2) + yOffset, v(:,3) + zOffset ...
+        ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
         end
-    end    
+    end
 end
-q0=robot.model.ikcon(a,qInitial);
-q1=robot.model.ikcon(b,qInitial);
-
-for i=1:steps
-    x(i,:) = (1-s(i))*a + s(i)*b;
+forwardTR = makehgtform('translate',[0,10,0]);
+posPaper = posPaper * forwardTR;
+updatedPt = [posPaper];
+for i=0:10
+    paperCircle([1]).Vert = updatedPt(:,1:3);
+    drawnow();
 end
-
-
 
