237ffb5b5df3637f0d1d6e2feb66884d12be19ee
Han Gyo Jung <pj1998paul@gmail.com>
2021-05-14 20:31:53 +1000

    update

--- environment/environment.m
+++ environment/environment.m
@@ -1,6 +1,6 @@
 clf;clc;clear all;close all;
 %doCameraSpin = false;
-
+%requires optimization tool box add on
 % Turn on a light (only turn on 1, don't keep turning them on), and make axis equal
 camlight;
 view(3)
